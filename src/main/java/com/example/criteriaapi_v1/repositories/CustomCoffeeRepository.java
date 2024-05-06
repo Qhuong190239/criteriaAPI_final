b0a592c993e633e8962d83d45b9fd878dad221dd
nqhuong <huongnqgch190239@fpt.edu.vn>
2024-05-06 11:08:20 +0700

    Update Rrepository Note

--- src/main/java/com/example/criteriaapi_v1/repositories/CustomCoffeeRepository.java
+++ src/main/java/com/example/criteriaapi_v1/repositories/CustomCoffeeRepository.java
@@ -10,19 +10,27 @@ import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-
+// Criteria API được sử dụng qua lớp này
 @Repository
 public class CustomCoffeeRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
     public Coffee getCoffeeById(long id) {
+        // Đầu tiên lấy đối tượng CriteriaBuilder từ EntityManager
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        // Tạo đối tượng CriteriaQuery để khai báo kiểu dữ liệu để truy vấn
         CriteriaQuery<Coffee> criteriaQuery = criteriaBuilder.createQuery(Coffee.class);
+        // Đối tượng Root được tạo để chỉ định đối tượng cơ sở của truy vấn
+        // khai báo đối tượng sẽ sử dụng trong query
         Root<Coffee> root = criteriaQuery.from(Coffee.class);
 
+        // Xây dựng truy vấn bằng cách sử dụng các phương thức của criteriaBuilder
+        // và các biểu thức như equal, like, and, or và nhiều phép toán khác
         Predicate predicate = criteriaBuilder.equal(root.get("id"), id);
 
+        // Câu lệnh select được dùng để lấy đối tượng đã khai báo
+        // và truyền vào đối tượng root
         criteriaQuery.select(root).where(predicate);
         return entityManager.createQuery(criteriaQuery).getSingleResult();
     }
